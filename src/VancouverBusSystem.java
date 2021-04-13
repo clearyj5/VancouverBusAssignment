53bb4573704306fd89750d780c98d46dd83f43eb
clearyj5 <clearyj5@tcd.ie>
2021-04-13 19:46:56 +0100

    Created project

--- src/VancouverBusSystem.java
+++ src/VancouverBusSystem.java
@@ -1,5 +1,21 @@
+import java.sql.Time;
+
 public class VancouverBusSystem {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Vancouver Bus System");
+    
+    public void findShortestRoute(String stop1, String stop2){
+
     }
+
+    public void findStopInfo(String stop){
+
+    }
+
+    public void findTripsWithArrivalTime(Time arrivalTime){
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Vancouver Bus System");
+    }
+
 }
