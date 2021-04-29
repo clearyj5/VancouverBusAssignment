2d6b1a30c68233d22d586127117bb030dd6e52db
clearyj5 <clearyj5@tcd.ie>
2021-04-29 16:24:51 +0100

    Final merge of master

--- src/TripTwo.java
+++ src/TripTwo.java
@@ -6,13 +6,9 @@ import java.util.Scanner;
 
 public class TripTwo {
 
-<<<<<<< HEAD
-	public static HashMap<String, ArrayList<String>> tripDataSet = new HashMap<String,ArrayList<String>>();  
-=======
 	public static HashMap<String, ArrayList<String>> tripDataSet = new HashMap<String, ArrayList<String>>();
 
 	TripTwo(String filename) {
->>>>>>> shortestRoutes
 
 		try {
 			Scanner inputScanner = new Scanner(new File(filename));
@@ -76,90 +72,44 @@ public class TripTwo {
 
 		ArrayList<String> tripsWithArrivalTime = new ArrayList<String>();
 
-<<<<<<< HEAD
-		if (isValidTime(arrivalTime)) {
-=======
 		String noBusError = "No buses depart at the specified time";
 		String invalidInputError = "the input '" + arrivalTime + "' is not a valid time";
 
-		if (isValidTime(arrivalTime)){
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
+		if (isValidTime(arrivalTime)) {
 
 			if (tripDataSet.containsKey(arrivalTime)) {
 				tripsWithArrivalTime = tripDataSet.get(arrivalTime);
-<<<<<<< HEAD
-<<<<<<< HEAD
-				 sortByID(tripsWithArrivalTime);
-=======
-
 				sortByID(tripsWithArrivalTime);
-
->>>>>>> shortestRoutes
-=======
-				sortByID(tripsWithArrivalTime);
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 			} else {
 				tripsWithArrivalTime.add(noBusError);
 			}
 		}
 
 		else {
-<<<<<<< HEAD
-<<<<<<< HEAD
-			tripsWithArrivalTime.add("the input '" + arrivalTime + "' is not a valid time" );
-=======
 			tripsWithArrivalTime.add(invalidInputError);
 		}
 
-		if(tripsWithArrivalTime.contains(noBusError) || tripsWithArrivalTime.contains(invalidInputError)){
+		if (tripsWithArrivalTime.contains(noBusError) || tripsWithArrivalTime.contains(invalidInputError)) {
 			return tripsWithArrivalTime;
-		}
-		else{
+		} else {
 			return formatOutput(tripsWithArrivalTime, arrivalTime);
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 		}
 	}
 
-
-<<<<<<< HEAD
-    //using insertion sort
-    public void sortByID(ArrayList<String> tripsWithArrivalTime){
-=======
-			tripsWithArrivalTime.add("the time '" + arrivalTime + "' is not a valid time");
-		}
-
-		return tripsWithArrivalTime;
-	}
->>>>>>> shortestRoutes
-
-	// using insertion sort
-	public void sortByID(ArrayList<String> tripsWithArrivalTime) {
-
-		int[] IDArray = new int[tripsWithArrivalTime.size()];
-
-		for (int index = 0; index < tripsWithArrivalTime.size(); index++) {
-
-			String id = tripsWithArrivalTime.get(index).split("\\,")[0];
-			id = id.trim();
-			int idN = Integer.parseInt(id);
-
-=======
-	public ArrayList<String> formatOutput(ArrayList<String> tripsWithArrivalTime, String arrivalTime){
-
-
+	public ArrayList<String> formatOutput(ArrayList<String> tripsWithArrivalTime, String arrivalTime) {
 
 		ArrayList<String> output = new ArrayList<String>();
 
-
-		for (int index =0; index<tripsWithArrivalTime.size(); index++){
+		for (int index = 0; index < tripsWithArrivalTime.size(); index++) {
 
 			String line = tripsWithArrivalTime.get(index);
 
-			String outputString = "Trip ID: " + line.split("\\,")[0] + ", Arrival Time: " + line.split("\\,")[1] + ", DepartureTime: " +
-					line.split("\\,")[2] + ", Stop ID: " + line.split("\\,")[3] + ", Stop sequence: "  + line.split("\\,") [4] + ", Stop headsign: " + line.split("\\,") [5] 
-							+ ", Pickup Type: " + line.split("\\,") [6] + ", Drop off type: "  + line.split("\\,") [7];
+			String outputString = "Trip ID: " + line.split("\\,")[0] + ", Arrival Time: " + line.split("\\,")[1]
+					+ ", DepartureTime: " + line.split("\\,")[2] + ", Stop ID: " + line.split("\\,")[3]
+					+ ", Stop sequence: " + line.split("\\,")[4] + ", Stop headsign: " + line.split("\\,")[5]
+					+ ", Pickup Type: " + line.split("\\,")[6] + ", Drop off type: " + line.split("\\,")[7];
 
-			if (line.split("\\,").length == 8){
+			if (line.split("\\,").length == 8) {
 
 				output.add(outputString);
 			}
@@ -172,27 +122,19 @@ public class TripTwo {
 
 		return output;
 
-
 	}
 
-
-
-
-
-
-	//using insertion sort
-	public void sortByID(ArrayList<String> tripsWithArrivalTime){
+	// using insertion sort
+	public void sortByID(ArrayList<String> tripsWithArrivalTime) {
 
 		int[] IDArray = new int[tripsWithArrivalTime.size()];
 
-
-		for (int index =0; index<tripsWithArrivalTime.size(); index++){
+		for (int index = 0; index < tripsWithArrivalTime.size(); index++) {
 
 			String id = tripsWithArrivalTime.get(index).split("\\,")[0];
 			id = id.trim();
 			int idN = Integer.parseInt(id);
 
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 			IDArray[index] = idN;
 		}
 
@@ -206,74 +148,32 @@ public class TripTwo {
 
 				if (IDArray[pHolder] > IDArray[pHolder + 1]) {
 
-<<<<<<< HEAD
 					int tempValue = IDArray[pHolder + 1];
-=======
-					String line = tripsWithArrivalTime.get(pHolder +1);
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 
 					String line = tripsWithArrivalTime.get(pHolder + 1);
 
-<<<<<<< HEAD
 					IDArray[pHolder + 1] = IDArray[pHolder];
 
-<<<<<<< HEAD
-                    tripsWithArrivalTime.set(pHolder, line);
-=======
 					tripsWithArrivalTime.set(pHolder + 1, tripsWithArrivalTime.get(pHolder));
 
 					tripsWithArrivalTime.set(pHolder, line);
-
->>>>>>> shortestRoutes
-=======
-					tripsWithArrivalTime.set(pHolder+1, tripsWithArrivalTime.get(pHolder));
-
-					tripsWithArrivalTime.set(pHolder, line);
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 				}
 
 				pHolder++;
 			} while (pHolder + 1 != IDArray.length);
 
 		}
-<<<<<<< HEAD
-=======
-
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 
 	}
 
-<<<<<<< HEAD
-
-=======
->>>>>>> shortestRoutes
 	public static void main(String[] args) {
 
 		TripTwo t2 = new TripTwo("stop_times.txt");
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-		for (int index =0; index<t2.findTripsWithArrivalTime("23:51:11").size(); index++){
-			System.out.println(t2.findTripsWithArrivalTime("23:51:11").get(index));
-=======
-		for (int index = 0; index < t2.findTripsWithArrivalTime("23:51:48").size(); index++) {
-			System.out.println(t2.findTripsWithArrivalTime("23:51:48").get(index));
->>>>>>> shortestRoutes
-		}
-
-=======
-		for (int index =0; index<t2.findTripsWithArrivalTime("5:25:00").size(); index++){
+		for (int index = 0; index < t2.findTripsWithArrivalTime("5:25:00").size(); index++) {
 
 			System.out.println(t2.findTripsWithArrivalTime("5:25:00").get(index));
 		}
 
-
-
-
-
-
-
->>>>>>> d8de3fd8f68028934c10bcfd3cade233cdf8fd63
 	}
 }
--- src/VancouverBusSystem.java
+++ src/VancouverBusSystem.java
@@ -1,24 +1,19 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> shortestRoutes
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
 import java.util.*;
 
+//most up to date
 public class VancouverBusSystem {
 
-
   public ArrayList<String> findShortestRoute(String stop1, String stop2) {
-    ArrayList<String> result = new ArrayList<String>();
+    ArrayList<String> result = new ArrayList<String>(); 
     result.add(stop1);
     result.add(stop2);
     return result;
   }
 
-<<<<<<< HEAD
   public ArrayList<String> findStopInfo(String stop) {
 
     /*
@@ -33,207 +28,82 @@ public class VancouverBusSystem {
      * -> trie(DST) key is names of the stops, value is the description
      */
 
-
     ArrayList<String> result = new ArrayList<String>();
     result.add(stop);
-    //result.add(stop2);
+    // result.add(stop2);
     return result;
 
   }
 
-    public void showMenu(TripTwo tt) {
+  public void showMenu(TripTwo tt) {
 
-      boolean shouldContinue = true;
-      Scanner sc = new Scanner(System.in);
-      do{
-
-        String[] userOptions = 
-        {"Please choose an option", 
-        "1. Find the shortest path between two stops", 
-        "2. Find bus stop by full name", 
-        "3. Find all trips with a given departure time"}; 
-
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setSize(420,420);
-
-
-        ImageIcon icon = new ImageIcon("NewBus.jpg"); 
-
-    
-
-
-        String inputFromUser = (String) JOptionPane.showInputDialog(
-
-          frame, 
-          "Which of Our Services Would You Like To Use? Select an Option From The Dropdown Menu", 
-          "Vancouver Bus Service", 
-          JOptionPane.WARNING_MESSAGE, 
-
-          icon, 
-
-          userOptions, 
-          userOptions[0]
-          );
-
-          switch(inputFromUser){
-              case "1. Find the shortest path between two stops":
-                String stop1 = JOptionPane.showInputDialog("Please enter the first stop"); 
-                String stop2 = JOptionPane.showInputDialog("Please enter the second stop"); 
-
-                  ArrayList<String> resultShortesRoute = findShortestRoute(stop1, stop2);
-                  String resultsshortest = "";
-                  for(String s : resultShortesRoute) {
-                    resultsshortest += s + "\n";
-                  }
-                JOptionPane.showMessageDialog(frame, resultsshortest);
-                  break;
-              case "2. Find bus stop by full name":
-              String stop = JOptionPane.showInputDialog("Please enter the stop name to search"); 
-              ArrayList<String> stopInfo = findStopInfo(stop);
-              String resultstopInfo = "";
-                  for(String s : stopInfo) {
-                    resultstopInfo += s + "\n";
-                  }
-                JOptionPane.showMessageDialog(frame, resultstopInfo);
-                  break;
-              case "3. Find all trips with a given departure time":
-                String timeByUser = JOptionPane.showInputDialog("Please enter the time you wish to depart in the format 'hh:mm:ss");
-                  ArrayList<String> arrivalTime = tt.findTripsWithArrivalTime(timeByUser);
-                  String resultToShow = "";
-                  for(String s : arrivalTime) {
-                    resultToShow += s + "\n";
-                  }
-                JOptionPane.showMessageDialog(frame, resultToShow);
-
-                default:
-              System.out.println("Invalid Input, try again!!");
-          }
-      } while(shouldContinue);
-
-
-  }
-
- 
-
-=======
-  public void findStopInfo(String stop) {
-
-  }
-  /*
-   * Searching for a bus stop by full name or by the first few characters in the
-   * name, using a ternary search tree (TST), returning the full stop information
-   * for each stop matching the search criteria (which can be zero, one or more
-   * stops) In order for this to provide meaningful search functionality please
-   * move keywords flagstop, wb, nb, sb, eb from start of the names to the end of
-   * the names of the stops when reading the file into a TST (eg “WB HASTINGS ST
-   * FS HOLDOM AVE” becomes “HASTINGS ST FS HOLDOM AVE WB”)
-   * 
-   * -> trie(DST) key is names of the stops, value is the description
-   */
-
-  public void showMenu() {
     boolean shouldContinue = true;
     Scanner sc = new Scanner(System.in);
     do {
 
       String[] userOptions = { "Please choose an option", "1. Find the shortest path between two stops",
-          "2. Find bus stop by full name", "3. Find all the trips of a given duration" };
+          "2. Find bus stop by full name", "3. Find all trips with a given departure time" };
 
       JFrame frame = new JFrame();
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setVisible(true);
       frame.setSize(420, 420);
 
-      ImageIcon icon = new ImageIcon("/Users/probbins/Desktop/bluebus.jpeg");
+      ImageIcon icon = new ImageIcon("NewBus.jpg");
 
       String inputFromUser = (String) JOptionPane.showInputDialog(
 
           frame, "Which of Our Services Would You Like To Use? Select an Option From The Dropdown Menu",
-          "*Vancouver Bus Service", JOptionPane.WARNING_MESSAGE, icon, userOptions, userOptions[0]);
+          "Vancouver Bus Service", JOptionPane.WARNING_MESSAGE,
+
+          icon,
+
+          userOptions, userOptions[0]);
 
       switch (inputFromUser) {
-      case "1. Find the shortest path between two stops":
-        String stop1 = JOptionPane.showInputDialog("Please enter the first stop");
-        String stop2 = JOptionPane.showInputDialog("Please enter the second stop");
-        findShortestRoute(stop1, stop2);
-        break;
-      case "2. Find bus stop by full name":
-        String stop = JOptionPane.showInputDialog("Please enter the stop name to search");
-        findStopInfo(stop);
-        break;
-      case "3. Find all the trips of a given duration":
-        String timeByUser = JOptionPane
-            .showInputDialog("Please enter the durtion of your trip in a hh:mm::ss format: ");
-        boolean isTimeValid = _isGivenTimeValid(timeByUser);
-        if (isTimeValid) {
-          BinarySearchTimes(timeByUser);
-        } else {
-          System.out.print("Invalid time entered, try again!!");
-        }
+        case "1. Find the shortest path between two stops":
+          String stop1 = JOptionPane.showInputDialog("Please enter the first stop");
+          String stop2 = JOptionPane.showInputDialog("Please enter the second stop");
 
-      default:
-        System.out.println("Invalid Input, try again!!");
+          ArrayList<String> resultShortesRoute = findShortestRoute(stop1, stop2);
+          String resultsshortest = "";
+          for (String s : resultShortesRoute) {
+            resultsshortest += s + "\n";
+          }
+          JOptionPane.showMessageDialog(frame, resultsshortest);
+          break;
+        case "2. Find bus stop by full name":
+          String stop = JOptionPane.showInputDialog("Please enter the stop name to search");
+          ArrayList<String> stopInfo = findStopInfo(stop);
+          String resultstopInfo = "";
+          for (String s : stopInfo) {
+            resultstopInfo += s + "\n";
+          }
+          JOptionPane.showMessageDialog(frame, resultstopInfo);
+          break;
+        case "3. Find all trips with a given departure time":
+          String timeByUser = JOptionPane
+              .showInputDialog("Please enter the time you wish to depart in the format 'hh:mm:ss");
+          ArrayList<String> arrivalTime = tt.findTripsWithArrivalTime(timeByUser);
+          String resultToShow = "";
+          for (String s : arrivalTime) {
+            resultToShow += s + "\n";
+          }
+          JOptionPane.showMessageDialog(frame, resultToShow);
+
+        default:
+          System.out.println("Invalid Input, try again!!");
       }
     } while (shouldContinue);
 
-  };
-
-  private boolean _isGivenTimeValid(String t) {
-    boolean timeValid = true;
-    try {
-      String[] hhmmss = t.split(":");
-
-      if (hhmmss.length < 3) {
-        timeValid = false;
-      }
-
-      int h = Integer.parseInt(hhmmss[0]);
-      int m = Integer.parseInt(hhmmss[1]);
-      int s = Integer.parseInt(hhmmss[2]);
-      if (h > 23 || h < 0) {
-        JOptionPane.showMessageDialog(null, "Invalid hours, it should be >=0 and <=23 ");
-        timeValid = false;
-      }
-      if (m < 0 || m > 59) {
-        JOptionPane.showMessageDialog(null, "Invalid mins, it should be >=0 and <=59 ");
-        timeValid = false;
-      }
-      if (s < 0 || s > 59) {
-        JOptionPane.showMessageDialog(null, "Invalid seconds, it should be >=0 and <=59 ");
-        timeValid = false;
-      }
-    } catch (Exception e) {
-      timeValid = false;
-    }
-    return timeValid;
   }
->>>>>>> shortestRoutes
 
   public static void main(String[] args) throws Exception {
     System.out.println("Welcome to Vancouver Bus System");
+    TripTwo t2 = new TripTwo("stop_times.txt");
+
     VancouverBusSystem v = new VancouverBusSystem();
-    v.showMenu();
+    v.showMenu(t2);
 
-    Trip tester = new Trip("stop_times.txt");
-
-    for (int index = 0; index < tester.distances.size(); index++) {
-
-<<<<<<< HEAD
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Vancouver Bus System");
-        TripTwo t2 = new TripTwo("stop_times.txt");
-
-        VancouverBusSystem v = new VancouverBusSystem();
-        v.showMenu(t2);
-
-        }
-      }
-=======
-      System.out.println(tester.distances.get(index) + " " + tester.stopIds.get(index));
->>>>>>> shortestRoutes
-
-    }
   }
 }
