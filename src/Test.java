49b5e72f7d6820558aa8533bf3958c672d0334c8
Hugo Crighton <hugocrighton@gmail.com>
2021-04-30 00:31:34 +0100

    implemented arraylist into test.java

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,35 @@
 import java.io.File;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+
         ReadFile keyValueFile = new ReadFile();
         TST tst = new TST();
         File stopInfo = new File("stops.txt");
 
-        String[] keyArray = keyValueFile.keyArray(stopInfo);
-        String[] valueArray = keyValueFile.valueArray(stopInfo);
+        ArrayList<String> keyArrayList = keyValueFile.keyArrayList(stopInfo);
+        ArrayList<String> valueArrayList = keyValueFile.valueArrayList(stopInfo);
 
-        for (int i = 0; i < 8758; i++) {
+        //System.out.println(keyArrayList.size());
+        for (int j = 0; j < keyArrayList.size(); j++) {
             String currentKey;
-            currentKey = keyArray[i];
+            currentKey = keyArrayList.get(j);
 
             String currentValue;
-            currentValue = valueArray[i];
+            currentValue = valueArrayList.get(j);
 
             tst.put(currentValue, currentKey);
 
         }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Stop name: ");
+        String search = scan.nextLine();
+        System.out.println(tst.get(search));
+
+        System.out.println("Stop prefix: ");
+        String prefix = scan.nextLine();
+        System.out.println(tst.valuesWithPrefix(prefix));
+
     }
 }
