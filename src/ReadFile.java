27bd6bce09e103c49e31c4659cd516da29035b36
Hugo Crighton <hugocrighton@gmail.com>
2021-04-30 00:33:10 +0100

    changed readfiles to arraylist, fixed string edditing bug

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,19 +1,14 @@
 import java.io.*; // Import the File/filereader class // Import this class to handle errors
 import java.util.*;
-import java.lang.*;
 
 public class ReadFile {
 
-  public static String[] keyArray = new String[8758];
-  // public static String[] valueArray = new String[70057];
+  public static ArrayList<String> keyArrayList = new ArrayList<String>();
+  public static ArrayList<String> valueArrayList = new ArrayList<String>();
 
-  public static void keyArray(String s, int i) {
-    keyArray[i] = s;
+  public static void keyArrayList(String s) {
+    keyArrayList.add(s);
   }
-
-  // public static void valueArray(String s, int i) {
-  // valueArray[i] = s;
-  // }
 
   /*
    * Takes all the adrress of the bus stops and places them into an array
@@ -21,19 +16,35 @@ public class ReadFile {
    * @param File the stops.txt file returns the array with all the stops address
    * catches file not found error
    */
-  public String[] keyArray(File file) {
+  public ArrayList<String> keyArrayList(File file) {
     try {
 
       Scanner myReader = new Scanner(file);
 
       myReader.nextLine();
-      int keyi = 1;
+      // int keyi = 1;
 
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
         String[] key = data.split(",");
-        keyArray(key[2], keyi);
-        keyi++;
+
+        if (key[2].contains("WB ")) {
+          key[2] = (editString(key[2]));
+        }
+        if (key[2].contains("FLAGSTOP ")) {
+          key[2] = editString(key[2]);
+        }
+        if (key[2].contains("NB ")) {
+          key[2] = (editString(key[2]));
+        }
+        if (key[2].contains("SB ")) {
+          key[2] = (editString(key[2]));
+        }
+        if (key[2].contains("EB ")) {
+          key[2] = (editString(key[2]));
+        } 
+          keyArrayList(key[2]);
+        
       }
       myReader.close();
     } catch (FileNotFoundException e) {
@@ -41,26 +52,112 @@ public class ReadFile {
       e.printStackTrace();
     }
 
-    return keyArray;
+    return keyArrayList;
   }
 
   /*
-  *Edits the position of the words in the keys
-  *by turning the strings into an element in an array and moving it
-  */
-  public String editString(String string) {
-    int index = string.indexOf("WB");
-    String[] aux = string.split(" ");
-    String temp = aux[index];
-    String temp2;
-    for (int i = 1; i < aux.length; i++) {
-      aux[i] = aux[i - 1];
+   * Edits the position of the words in the keys by turning the strings into an
+   * element in an array and moving it
+   */
+  public static String editString(String string) {
+    if (string.contains("WB ")) {
+      // converts string to array
+      String[] aux = string.split(" ");
+      int index = -1;
+      // finds the index of WB
+      for (int i = 0; i < aux.length; i++) {
+        if (aux[i].equals("WB")) {
+          index = i;
+          break;
+        }
+      }
+      String temp = aux[index];
+      for (int j = 1; j < aux.length; j++) {
+        aux[j - 1] = aux[j];
+      }
+      int length = aux.length - 1;
+      aux[length] = temp;
+      String appended = Arrays.toString(aux);
+      return appended;
     }
-    int length = aux.length - 1;
-    aux[length] = temp;
-    String appended = Arrays.toString(aux);
 
-    return appended;
+    else if (string.contains("FLAGSTOP ")) {
+      String[] aux = string.split(" ");
+      int index = -1;
+      for (int i = 0; i < aux.length; i++) {
+        if (aux[i].equals("FLAGSTOP")) {
+          index = i;
+          break;
+        }
+      }
+      String temp = aux[index];
+      for (int j = 1; j < aux.length; j++) {
+        aux[j - 1] = aux[j];
+      }
+      int length = aux.length - 1;
+      aux[length] = temp;
+      String appended = Arrays.toString(aux);
+      return appended;
+    }
+
+    else if (string.contains("NB ")) {
+      String[] aux = string.split(" ");
+      int index = -1;
+      for (int i = 0; i < aux.length; i++) {
+        if (aux[i].equals("NB")) {
+          index = i;
+          break;
+        }
+      }
+      String temp = aux[index];
+      for (int j = 1; j < aux.length; j++) {
+        aux[j - 1] = aux[j];
+      }
+      int length = aux.length - 1;
+      aux[length] = temp;
+      String appended = Arrays.toString(aux);
+      return appended;
+    }
+
+    else if (string.contains("SB ")) {
+      String[] aux = string.split(" ");
+      int index = -1;
+      for (int i = 0; i < aux.length; i++) {
+        if (aux[i].equals("SB")) {
+          index = i;
+          break;
+        }
+      }
+      String temp = aux[index];
+      for (int j = 1; j < aux.length; j++) {
+        aux[j - 1] = aux[j];
+      }
+      int length = aux.length - 1;
+      aux[length] = temp;
+      String appended = Arrays.toString(aux);
+      return appended;
+    }
+
+    else if (string.contains("EB ")) {
+      String[] aux = string.split(" ");
+      int index = -1;
+      for (int i = 0; i < aux.length; i++) {
+        if (aux[i].equals("EB")) {
+          index = i;
+          break;
+        }
+      }
+      String temp = aux[index];
+      for (int j = 1; j < aux.length; j++) {
+        aux[j - 1] = aux[j];
+      }
+      int length = aux.length - 1;
+      aux[length] = temp;
+      String appended = Arrays.toString(aux);
+      return appended;
+    } else {
+      return string;
+    }
   }
 
   /*
@@ -69,44 +166,26 @@ public class ReadFile {
    * @param File the stops.txt file returns the array with all the stops info
    * catches file not found error
    */
-  public String[] valueArray(File file) {
-    String[] value = new String[8758];
+  public ArrayList<String> valueArrayList(File file) {
     try {
 
       Scanner myReader = new Scanner(file);
       myReader.nextLine();
-      int valuei = 1;
+      // int valuei = 1;
 
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        if (data.contains("WB ")) {
-          editString(data);
-        }
-        if (data.contains("FLAGSTOP ")) {
-          editString(data);
-        }
-        if (data.contains("NB ")) {
-          editString(data);
-        }
-        if (data.contains("SB ")) {
-          editString(data);
-        }
-        if (data.contains("EB ")) {
-          editString(data);
-        }
-        value[valuei] = data;
-        valuei++;
-
+        valueArrayList.add(data);
       }
       myReader.close();
     } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-    for (int i = 0; i < 8758; i++) {
-      System.out.println(value[i]);
-    }
-    return value;
+    // for (int i = 0; i < valueArrayList.size(); i++) {
+    //   System.out.println(valueArrayList.get(i));
+    // }
+    return valueArrayList;
   }
 
   public static void main(String[] args) {
