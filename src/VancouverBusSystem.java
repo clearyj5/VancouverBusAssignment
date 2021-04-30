0f5ce0f414aec7c051d9f1e57fe395bcac7964ef
Hugo Crighton <hugocrighton@gmail.com>
2021-04-30 17:10:16 +0100

    removed comment

--- src/VancouverBusSystem.java
+++ src/VancouverBusSystem.java
@@ -15,18 +15,6 @@ public class VancouverBusSystem {
   }
 
   public ArrayList<String> findStopInfo(String stop) {
-
-    /*
-     * Searching for a bus stop by full name or by the first few characters in the
-     * name, using a ternary search tree (TST), returning the full stop information
-     * for each stop matching the search criteria (which can be zero, one or more
-     * stops) In order for this to provide meaningful search functionality please
-     * move keywords flagstop, wb, nb, sb, eb from start of the names to the end of
-     * the names of the stops when reading the file into a TST (eg “WB HASTINGS ST
-     * FS HOLDOM AVE” becomes “HASTINGS ST FS HOLDOM AVE WB”)
-     * 
-     * -> trie(DST) key is names of the stops, value is the description
-     */
     TST tst = new TST();
     ArrayList<String> result = new ArrayList<String>();
     result = tst.valuesWithPrefix(stop);
